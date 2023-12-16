98ff1ed7b0fc70ddf0b1214a8898c8d1c96380d3
MDsub <98559918+MDsub@users.noreply.github.com>
2023-12-16 22:07:37 +0000

    整理

--- pending/fix_link.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 遍历当前目录下的所有.md文件
-for file in *.md
-do
-  # 使用sed命令和正则表达式替换文件中的字符串
-  sed -E -i '' 's/（提取码：(.{4})）/?pwd=\1/g' "$file"
-  sed -E -i '' 's@\| (https://[^ ]+) @|[链接](\1) @g' "$file"
-done
